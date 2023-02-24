ee902120723ef85cf3c6d2519256965127a0ba3d
Ziad Dorrah <ziadadelhassan341@gmail.com>
2023-02-24 15:42:46 +0200

    edit Archive
    add some hover and add footer and Scroll Button

--- src/js/archive.js
+++ src/js/archive.js
@@ -31,3 +31,23 @@ function getrepos() {
       });
     });
 }
+//Button Scroll
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 800 ||
+    document.documentElement.scrollTop > 800
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
