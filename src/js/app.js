411711bddc0aa5df8d5585893a4a73f987edfb2d
Ziad Dorrah <ziadadelhassan341@gmail.com>
2023-04-07 07:33:04 +0200

    Updates
    make index.html more simple

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,34 @@
+let about = document.querySelector(".about");
+let work = document.querySelector(".work");
+let contact = document.querySelector(".contact");
+let morebtn = document.querySelector(".more");
+// let workrow = document.querySelectorAll(".work .row > div:nth-last-child(n+7)")
+
+// morebtn.addEventListener("click",()=>{
+//   for (let i =0; i < workrow.length; i++){
+//     workrow[i].style.display = "block"
+//   }
+// })
+
+window.addEventListener("scroll", () => {
+  const sectionabout = about.getBoundingClientRect();
+  const sectionwork = work.getBoundingClientRect();
+  const sectioncontact = contact.getBoundingClientRect();
+  if (sectionabout.top <= window.innerHeight && sectionabout.bottom >= 0){
+    about.classList.add("active")
+  }
+  if (sectionwork.top <= window.innerHeight && sectionwork.bottom >= 0){
+    work.classList.add("active")
+  }
+  if (sectioncontact.top <= window.innerHeight && sectioncontact.bottom >= 0){
+    contact.classList.add("active")
+  }
+  
+});
+
+window.onscroll = () => {
+  console.log(clientHeight);
+};
 window.addEventListener("load", () => {
   let reload = document.querySelector(".preloader");
   setTimeout(() => {
@@ -14,8 +45,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 800 ||
-    document.documentElement.scrollTop > 800
+    document.body.scrollTop > 650 ||
+    document.documentElement.scrollTop > 650
   ) {
     mybutton.style.display = "block";
   } else {
